52a43b746e8b09916b1058ebaeb738bfefcb7ea5
sunny-rajak <sunnyrajak257@gmail.com>
2025-07-08 22:51:22 +0530

    added vite_api_base_url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,10 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    server: {
-        port: 3000,
-    },
-    build: {
-        outDir: "dist",
-    },
 });
